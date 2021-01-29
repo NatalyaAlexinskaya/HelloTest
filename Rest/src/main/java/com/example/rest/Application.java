304d2baef9ed9ecf79ce0f2dd8398e238c85888e
Sergey <skosterin@avaya.com>
2021-01-29 16:06:41 +0300

    changes

--- Rest/src/main/java/com/example/rest/Application.java
+++ Rest/src/main/java/com/example/rest/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.example.kafka", "com.example.service"})
+@ComponentScan({"com.example.rest", "com.example.service"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
